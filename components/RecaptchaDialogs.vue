921778941d9667c5684e2955697cd324cc706a0e
krpk1900 <krpk1900@gmail.com>
2021-06-04 18:54:01 +0900

    [Add] 画像を3*3で表示するように実装

--- components/RecaptchaDialogs.vue
+++ components/RecaptchaDialogs.vue
@@ -1,17 +1,38 @@
 <template>
-  <v-dialog v-model="privacyPolicy" width="500" scrollable>
+  <v-dialog v-model="privacyPolicy" width="550" scrollable>
     <template v-slot:activator="{ on, attrs }">
       <a v-bind="attrs" v-on="on" style="color: #099e9e;">Privacy Policy</a>
     </template>
     <v-card>
-      <v-card-title class="light-blue text-h4 font-weight-black white--text">スキマスイッチ</v-card-title>
-      <v-card-subtitle class="light-blue font-weight-black white--text">のタイルをすべて選択してください</v-card-subtitle>
-      <v-card-text>
-        aaa
-      </v-card-text>
+      <v-card-title class="light-blue text-h3 font-weight-black white--text">ベーグル</v-card-title>
+      <v-card-subtitle class="light-blue text-body-1 font-weight-black white--text">
+        のタイルをすべて選択してください。<br>
+        すべて選択し終わったら[確認]をクリックしてください。
+      </v-card-subtitle>
+      <v-card-actions>
+        <img src="bagel1.jpg" width="150px" class="picture">
+        <img src="dog1.jpg" width="150px" class="picture">
+        <img src="bagel2.jpg" width="150px" class="picture">
+      </v-card-actions>
+      <v-card-actions>
+        <img src="dog2.jpg" width="150px" class="picture">
+        <img src="bagel3.jpg" width="150px" class="picture">
+        <img src="dog3.jpg" width="150px" class="picture">
+      </v-card-actions>
+      <v-card-actions>
+        <img src="bagel4.jpg" width="150px" class="picture">
+        <img src="dog4.jpg" width="150px" class="picture">
+        <img src="bagel5.jpg" width="150px" class="picture">
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
+
+<style>
+  .picture {
+    margin: 10px;
+  }
+</style>
 
 <script>
 export default {
