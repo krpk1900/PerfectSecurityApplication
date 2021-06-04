32a8ad1d1f9c6515622dfd62ca747fab5bda76fd
krpk1900 <krpk1900@gmail.com>
2021-06-04 15:47:49 +0900

    [Add] 何人目の訪問者か表示する機能を完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <button
-      type="button"
-      @click="submit()"
-  >
-    送信
-  </button>
+  <div>
+    <button type="button" @click="submit(); dataCount()">送信</button>
+    <p>あなたは世界一堅牢なセキュリティを突破した{{ count }}人目のハッカーです。</p>
+  </div>
 
 </template>
 
@@ -13,6 +11,9 @@ import firebase from '@/plugins/firebase'
 let database = firebase.database()
 export default {
   data () {
+    return {
+      count: 0,
+    }
   },
   methods: {
     submit () {
@@ -20,6 +21,12 @@ export default {
         id: "id",
       });
       console.log('送信しました')
+    },
+    dataCount () {
+      database.ref("users").once('value', parent => {
+        this.count = parent.numChildren()
+        console.log(this.count)
+      });
     }
   },
 }
