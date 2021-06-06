11fd8b81b3df35c037a737061d8247a88f7aca8b
krpk1900 <krpk1900@gmail.com>
2021-06-06 19:02:14 +0900

    [Add] コメントを追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,22 @@
   <div>
     <button type="button" @click="submit(); dataCount()">送信</button>
     <p>あなたは世界一堅牢なセキュリティを突破した{{ count }}人目のハッカーです。</p>
+    <!-- ステージ1 (ベーグルと犬) -->
     <RecaptchaDialogs
     target-name='ベーグル' stage-num='1' image-name1='bagel1.jpg' image-name2='dog1.jpg' image-name3='bagel2.jpg' image-name4='dog2.jpg'
     image-name5='bagel3.jpg' image-name6='dog3.jpg' image-name7='bagel4.jpg' image-name8='dog4.jpg' image-name9='bagel5.jpg' @success="isShowDialogs2 = true">
     </RecaptchaDialogs>
-
+    <!-- ステージ2 (チワワとマフィン) -->
     <RecaptchaDialogs v-if="isShowDialogs2"
     target-name='チワワ' stage-num='2' image-name1='chihuahua1.jpg' image-name2='muffin1.jpg' image-name3='chihuahua2.jpg' image-name4='muffin2.jpg'
     image-name5='chihuahua3.jpg' image-name6='muffin3.jpg' image-name7='chihuahua4.jpg' image-name8='muffin4.jpg' image-name9='chihuahua5.jpg' @success="isShowDialogs3 = true">
     </RecaptchaDialogs>
-
+    <!-- ステージ3 (モップとシープドッグ) -->
+    <RecaptchaDialogs v-if="isShowDialogs3"
+    target-name='モップ' stage-num='3' image-name1='mop1.jpg' image-name2='sheepdog1.jpg' image-name3='mop2.jpg' image-name4='sheepdog2.jpg'
+    image-name5='mop3.jpg' image-name6='sheepdog3.jpg' image-name7='mop4.jpg' image-name8='sheepdog4.jpg' image-name9='mop5.jpg' @success="isShowDialogs4 = true">
+    </RecaptchaDialogs>
+    <!-- ステージ4 (モップとシープドッグ) -->
     <RecaptchaDialogs v-if="isShowDialogs3"
     target-name='モップ' stage-num='3' image-name1='mop1.jpg' image-name2='sheepdog1.jpg' image-name3='mop2.jpg' image-name4='sheepdog2.jpg'
     image-name5='mop3.jpg' image-name6='sheepdog3.jpg' image-name7='mop4.jpg' image-name8='sheepdog4.jpg' image-name9='mop5.jpg' @success="isShowDialogs4 = true">
